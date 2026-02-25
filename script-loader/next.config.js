e01ab295d5de1bd735b0236bf5b88d591b39c968
zenee717-byte <zenee717@gmail.com>
2026-02-25 07:20:54 +0700

    Delete script-loader directory

--- script-loader/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Prevent data/ from being served as static files
-  // Script lives server-side only
-};
-
-module.exports = nextConfig;
